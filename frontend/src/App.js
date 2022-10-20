de56d64c2a5d80f088922514ee69af108aba6621
Dillon Hess <dhess268@gmail.com>
2022-10-19 22:36:32 -0400

    map and search bar
    The map has the ability to have markers now based on a data object and the markers can have content in them. There's also a search feature to pan the map to different places

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,94 @@
-import { useMemo } from 'react';
-import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+import { useMemo, useRef, useState } from 'react';
+import { GoogleMap, useLoadScript, Autocomplete } from '@react-google-maps/api';
+import MarkerWithInfoWindow from './MarkerWithInfoWindow';
 
+const markerData = [
+  { pos: { lat: 44, lng: -80 }, content: 'hi there' },
+  { pos: { lat: 41.2709, lng: -73.7776 }, content: 'hi there' },
+];
+const libraries = ['places'];
 function App() {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY_A,
+    libraries,
   });
-
   if (!isLoaded) return <div className="App">Loading...</div>;
   return <Map />;
 }
 
 function Map() {
-  const center = useMemo(() => ({ lat: 44, lng: -80 }));
+  const center = useMemo(() => ({ lat: 44, lng: -80 }), []);
+  const center2 = useMemo(() => ({ lat: 41.2709, lng: -73.7776 }), []);
+  const [center3, setCenter3] = useState(center2);
+  const center4 = useRef(center);
+  const [inputValue, setInputValue] = useState('');
+  const [autocomplete, setAutocomplete] = useState(null);
+  function onLoad(auto) {
+    setAutocomplete(auto);
+  }
+  async function onPlaceChanged() {
+    if (autocomplete !== null) {
+      console.log(autocomplete.getPlace().geometry.location.lat());
+      console.log(autocomplete.getPlace().geometry.location.lng());
+      setCenter3({
+        lat: autocomplete.getPlace().geometry.location.lat(),
+        lng: autocomplete.getPlace().geometry.location.lng(),
+      });
+      setInputValue('');
+    } else {
+      console.log('Autocomplete is not loaded yet!');
+    }
+  }
+
   return (
     <GoogleMap
-      zoom={10}
-      center={center}
-      mapContainerClassName="map-container"
-    />
+      zoom={16}
+      center={center3}
+      mapContainerStyle={{
+        height: '100vh',
+        width: '100%',
+      }}
+      onClick={(e) => console.log(e.latLng.lat())}
+    >
+      <Autocomplete
+        onLoad={(auto) => onLoad(auto)}
+        onPlaceChanged={() => onPlaceChanged()}
+      >
+        <input
+          type="text"
+          placeholder="Customized your placeholder"
+          style={{
+            boxSizing: `border-box`,
+            border: `1px solid transparent`,
+            width: `240px`,
+            height: `32px`,
+            padding: `0 12px`,
+            borderRadius: `3px`,
+            boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+            fontSize: `14px`,
+            outline: `none`,
+            textOverflow: `ellipses`,
+            position: 'absolute',
+            left: '50%',
+            marginLeft: '-120px',
+          }}
+          onChange={(e) => setInputValue(e.value)}
+          value={inputValue}
+        />
+      </Autocomplete>
+      {/* <MarkerWithInfoWindow position={center} content="Toronto" />
+      <MarkerWithInfoWindow
+        position={center2}
+        content="Yorktown Heights yaya"
+      /> */}
+      {markerData.map((marker, i) => (
+        <MarkerWithInfoWindow
+          position={marker.pos}
+          content={marker.content}
+          key={i}
+        />
+      ))}
+    </GoogleMap>
   );
 }
 
