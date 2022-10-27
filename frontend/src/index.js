2d4fc70640b8135c6c91aa3517fa162eb4026da0
Dillon Hess <dhess268@gmail.com>
2022-10-27 16:46:21 -0400

    hash router time

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
 import Landing from './containers/Landing';
 import ErrorPage from './ErrorPage';
@@ -12,7 +12,7 @@ import AnimalList from './components/AnimalLists';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/map" element={<App />} />
@@ -21,6 +21,6 @@ root.render(
         <Route path="/listings" element={<AnimalList />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
