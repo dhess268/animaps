8daec9e5e0a2b25a7998be222119dc7e09729098
Dillon Hess <dhess268@gmail.com>
2022-11-30 22:21:51 -0500

    fix server code

--- backend/util/validateData.js
+++ backend/util/validateData.js
@@ -8,7 +8,7 @@ const validateNewUser = (req) => {
 }
 
 const validateUpdateUser = (req) => {
-  if(req.body.username !== '' && req.body.firstname !== '' && req.body.lastname !== '' && req.body.email !== '' && req.body.password !== '' && req.body.addressString && req.body.addressLatLng) {
+  if(req.body.firstname !== '' && req.body.lastname !== ''  && req.body.addressString && req.body.addressLatLng) {
     return true
   } else {
     return false
